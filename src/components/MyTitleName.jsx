e2479cc3bd73f7ffeadfcd96f04b2804a6276310
Champ <droped17@gmail.com>
2024-01-04 14:12:39 +0700

    edit HomePage UI

--- src/components/MyTitleName.jsx
+++ src/components/MyTitleName.jsx
@@ -1,6 +1,6 @@
 export default function MyTitleName({title}){
     return (
-        <div className="text-center p-2">
+        <div className="text-center p-2 mb-3">
             <span className="border-b-4 p-2 border-red-500 font-bold text-xl">{title}</span>
         </div>
     );
